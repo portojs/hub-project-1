b9d7538f4ea266ffc0b89d516730c0700e56670b
peter <peter.k.nets@gmail.com>
2016-01-20 18:09:08 +0200

    Add some error handling code

--- app/01_node/app.js
+++ app/01_node/app.js
@@ -2,11 +2,18 @@
  * Created by Peter on 18.01.2016.
  */
 var https = require("https");
+var http = require("http");
 var username = "peterkrevenets";
 
+//Print out messages
 function printMessage(username, badgeCount, points) {
-  var message = username + " has " + badgeCount + " total badge(s) and " + points + " in JavaScript";
+  var message = "User " + username + " has " + badgeCount + " total badge(s) and " + points + " in JavaScript";
   console.log(message);
+}
+
+//Print our error messages
+function printError(error) {
+  console.error(error.message);
 }
 
 //Connect to the API URL (https://teamtreehouse.com/username.json)
@@ -17,13 +24,22 @@ var request = https.get("https://teamtreehouse.com/" + username + ".json", funct
     body += chunk;
   });
   response.on("end", function() {
-    var profile = JSON.parse(body);
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
+    if (response.statusCode === 200) {
+      try {
+        var profile = JSON.parse(body);
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+      } catch(error) {
+        //Parse error
+        printError(error);
+      }
+    } else {
+      //Status Code error
+      printError({message: "There was an error getting the profile for " + username + " (" + http.STATUS_CODES[response.statusCode] + ")"});
+    }
   });
   //Parse the data
   //Print the data
 });
 
-request.on("error", function(error) {
-  console.error(error.message);
-});
+//Connection error
+request.on("error", printError);
