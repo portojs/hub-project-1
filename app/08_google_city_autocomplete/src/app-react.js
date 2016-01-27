e1b5cb7260fc7d0cfec95c0e0736018724c638c7
peter <peter.k.nets@gmail.com>
2016-01-27 17:49:29 +0200

    Express: beautiful site

--- app/08_google_city_autocomplete/src/app-react.js
+++ app/08_google_city_autocomplete/src/app-react.js
@@ -6,21 +6,36 @@
 (function() {
   'use strict';
 
+  var Highlight = {
+    componentDidUpdate: function() {
+      var node = $(this.getDOMNode());
+      node.slideUp();
+      node.slideDown();
+    }
+  };
   var bookList = ["The Hobbit", "The Wheel of Time"];
   var Quiz = React.createClass({
+    propTypes: {
+      books: React.PropTypes.array.isRequired
+    },
     render: function() {
       return <div>
         {this.props.books.map(function(book) {
           return <Book title={book} />
         })}
         </div>;
-    }
+    },
+    mixins: [Highlight]
   });
 
   var Book = React.createClass({
+    propTypes: {
+      title: React.PropTypes.string.isRequired
+    },
     render: function() {
       return <div><h4>{this.props.title}</h4></div>;
-        }
+    },
+    mixins: [Highlight]
   });
 
   React.render(<Quiz books={bookList}/>,
