c999f02c0661b6ebbf5c5fa82a31e7331d40b3c5
peter <peter.k.nets@gmail.com>
2016-02-10 13:31:53 +0200

    Foundation: add more content and use columns

--- app/11_react_geosuggest/src/app.js
+++ app/11_react_geosuggest/src/app.js
@@ -1,40 +1,37 @@
-/**
- * Created by Peter on 06.02.2016.
- */
+var React = require('react'),
+  Geosuggest = require('./src/Geosuggest.jsx');
 
-(function() {
-  'use strict';
+var App = React.createClass({
+  /**
+   * Render the example app
+   */
+  render: function() {
+    var fixtures = [
+      {label: 'Old Elbe Tunnel, Hamburg', location: {lat: 53.5459, lng: 9.966576}},
+      {label: 'Reeperbahn, Hamburg', location: {lat: 53.5495629, lng: 9.9625838}},
+      {label: 'Alster, Hamburg', location: {lat: 53.5610398, lng: 10.0259135}}
+    ];
 
-  //var Geosuggest = require('react-geosuggest'),
-  //         React = require('react');
-  require(['require', 'react-geosuggest'], function(require) {
-    var Geosuggest = require('react-geosuggest');
-  });
+    return (
+      <div>
+        <Geosuggest
+          placeholder="Start typing!"
+          initialValue="Hamburg"
+          fixtures={fixtures}
+          onSuggestSelect={this.onSuggestSelect}
+          location={new google.maps.LatLng(53.558572, 9.9278215)}
+          radius="20" />
+      </div>
+    )
+  },
 
-  var App = React.createClass({
-    render: function() {
-      var fixtures = [
-        {label: 'Old Elbe Tunnel, Hamburg', location: {lat: 53.5459, lng: 9.966576}},
-        {label: 'Reeperbahn, Hamburg', location: {lat: 53.5495629, lng: 9.9625838}},
-        {label: 'Alster, Hamburg', location: {lat: 53.5610398, lng: 10.0259135}}
-      ];
+  /**
+   * When a suggest got selected
+   * @param  {Object} suggest The suggest
+   */
+  onSuggestSelect: function(suggest) {
+    console.log(suggest);
+  }
+});
 
-      return (
-        <div>
-          <Geosuggest
-            placeholder="Start typing!"
-            initialValue="Hamburg"
-            fixtures={fixtures}
-            onSuggestSelect={this.onSuggestSelect}
-            location={new google.maps.LatLng(53.558572, 9.9278215)}
-            radius="20" />
-        </div>
-      )
-    },
-    onSuggestSelect: function(suggest) {
-      console.log(suggest);
-    }
-  });
-
-  React.render(<App />, document.getElementById('app'));
-})();
+React.render(<App />, document.getElementById('app'));
