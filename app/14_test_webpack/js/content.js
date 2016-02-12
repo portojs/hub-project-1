fe97d98760ee335607b7405d0c6949c2a5bd6ab0
peter <peter.k.nets@gmail.com>
2016-02-12 12:45:13 +0200

    Webpack: add css-loader and style-loader

---
+++ app/14_test_webpack/js/content.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Peter on 12.02.2016.
+ */
+module.exports = "It works in entry.js.";
---
+++ app/14_test_webpack/js/entry.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Peter on 12.02.2016.
+ */
+document.write(require('./content.js'));
